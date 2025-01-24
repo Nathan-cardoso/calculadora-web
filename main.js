a707f3c234acb83c1698bd09372be6d415210e8d
Nathan-cardoso <nathancar.linhares77@gmail.com>
2025-01-23 22:51:49 -0300

    feat: add as operações

--- main.js
+++ main.js
@@ -23,10 +23,63 @@ function dividir(a,b){
 
 const numbers = document.querySelectorAll('.number-botton');
 const painel = document.querySelector('.calc');
+const printresult = document.querySelector('.result');
 const operation = document.querySelectorAll('.operation-botton');
-const equal = document.querySelector('.equals-botton');
+const equals = document.querySelector('.equals-botton');
 
 
+let firstNumber = '';
+let secondNumber = '';
+let operator = '';
+let isSecondNumber = false;
+
+
+numbers.forEach((button) => {
+    button.addEventListener('click', () =>{
+        if(isSecondNumber){
+            secondNumber += button.textContent;
+            painel.innerHTML = secondNumber;
+        }else{
+            firstNumber += button.textContent;
+            painel.innerHTML = firstNumber;
+        }
+    })
+})
+
+operation.forEach((bottonOperator) =>{
+    bottonOperator.addEventListener('click', () =>{
+        operator = bottonOperator.innerHTML;
+        painel.innerHTML = '';
+        isSecondNumber = true;
+    })
+})
+
+equals.addEventListener('click', () =>{
+    let result 
+
+    switch (operator){
+        case "+":
+            result = somar(parseFloat(firstNumber), parseFloat(secondNumber));
+        break; 
+        case "-": 
+            result = subtrair(parseFloat(firstNumber), parseFloat(secondNumber));
+        break; 
+        case "x" || "*": 
+            result = multiplicar(parseFloat(firstNumber), parseFloat(secondNumber));
+        break; 
+        case "/":
+            result = dividir(parseFloat(firstNumber), parseFloat(secondNumber));
+        break; 
+    }
+
+    painel.innerHTML = '';
+    result === 'NaN' ? printresult.innerHTML = '' : printresult.innerHTML = result;
+    firstNumber = result.toString();
+    secondNumber = '';
+    operator = '';
+    isSecondNumber = false;
+
+})
 
 
 
